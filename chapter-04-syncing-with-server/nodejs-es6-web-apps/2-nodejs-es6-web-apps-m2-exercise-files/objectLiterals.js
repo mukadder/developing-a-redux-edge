9a4adb024044f430cf95ff4183eb38414a346533
mukadder <hht179@gmail.com>
2016-12-18 22:12:46 -0500

    cc

---
+++ chapter-04-syncing-with-server/nodejs-es6-web-apps/2-nodejs-es6-web-apps-m2-exercise-files/objectLiterals.js
@@ -0,0 +1,19 @@
+
+
+function greetingService(name){
+	var sayHi = function(){
+		console.log('Hello '+ name + '!');
+	}
+	var sayBye = function(){
+		console.log('Goodbye '+ name + '!');
+	}
+	return {
+		sayHi,
+		sayBye
+	}
+}
+
+var gs = greetingService('Jon');
+
+gs.sayHi();
+gs.sayBye();
